5fecd88e980c10633373812ae3c3113b44611843
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2023-07-03 19:50:12 -0600

    aa

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else{
-            entry.target.classList.remove('show');
-        }
-    })
-});
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else{
+//             entry.target.classList.remove('show');
+//         }
+//     })
+// });
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
